59222dea7917a1274f438c982eda5672099e45fe
Emi <e.wong@hotmail.co.uk>
2021-09-20 11:28:59 +0100

    add css to header and nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,12 @@
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUsers } from '@fortawesome/free-solid-svg-icons';
-import { useContext } from 'react';
-import { UserContext } from '../contexts/User';
+import './Header.css';
 
 const Header = () => {
-  const { user } = useContext(UserContext);
-
   return (
     <header className='Header'>
       <h1>
-        <Link to='/'>
+        <Link className='title' to='/'>
           <span>Game Reviews</span>
-        </Link>
-        <Link className='user__corner' to='/users'>
-          <span>{user.username}</span>
-          <span>
-            <FontAwesomeIcon icon={faUsers} />
-          </span>
         </Link>
       </h1>
     </header>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,26 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../contexts/User';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import './Nav.css';
 
 const Nav = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <nav className='Nav'>
-      <Link to='/categories' key='categories'>
+      <Link to='/categories' key='categories' className='link'>
         Categories
       </Link>
-      <Link to='/reviews' key='reviews'>
+      <Link to='/reviews' key='reviews' className='link'>
         Reviews
+      </Link>
+      <Link className='user' to='/users'>
+        <span>{user.username}</span>
+        <span className='icon'>
+          <FontAwesomeIcon icon={faUsers} />
+        </span>
       </Link>
     </nav>
   );
